e3810fd2bf49b78c40392f313faacd10aa619687
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-12-19 14:31:32 +0530

    Removing username and password fields

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/conf/OAuthEndpoint.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/conf/OAuthEndpoint.java
@@ -6,8 +6,6 @@ public class OAuthEndpoint {
     private String tokenApiUrl;
     private String apiKey;
     private String apiSecret;
-    private String username;
-    private String password;
     private String grantType;
     private int tokenRefreshInterval;
 
@@ -41,22 +39,6 @@ public class OAuthEndpoint {
 
     public void setApiSecret(String apiSecret) {
         this.apiSecret = apiSecret;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getGrantType() {
