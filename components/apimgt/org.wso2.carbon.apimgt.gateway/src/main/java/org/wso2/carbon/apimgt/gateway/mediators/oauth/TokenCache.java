ba14e696672cc325ecbf795e74174ac978428be3
Sanjula Madurapperuma <sanjula99@gmail.com>
2020-01-24 11:25:45 +0530

    Adding simple caching mechanism for OAuth Endpoint Security

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/TokenCache.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/TokenCache.java
@@ -1,4 +1,35 @@
 package org.wso2.carbon.apimgt.gateway.mediators.oauth;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Token Cache Implementation
+ */
 public class TokenCache {
+    private static final TokenCache instance = new TokenCache();
+
+    private final Map<String, String> tokenMap = new HashMap<>();
+
+    /**
+     * Private Constructor
+     */
+    private TokenCache() {
+    }
+
+    /**
+     * Get TokenCache Instance
+     * @return
+     */
+    public static TokenCache getInstance() {
+        return instance;
+    }
+
+    /**
+     * Get token map
+     * @return
+     */
+    public Map<String, String> getTokenMap() {
+        return tokenMap;
+    }
 }
