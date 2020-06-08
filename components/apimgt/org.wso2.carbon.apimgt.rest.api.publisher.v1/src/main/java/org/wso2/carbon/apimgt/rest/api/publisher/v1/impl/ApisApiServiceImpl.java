002ccadc861afd77b8cb1e135c1edad469474873
Sanjula Madurapperuma <sanjula99@gmail.com>
2020-06-08 21:39:28 +0530

    Removing duplicate object reference

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -680,6 +680,7 @@ public class ApisApiServiceImpl implements ApisApiService {
 
 
             LinkedHashMap endpointConfig = (LinkedHashMap) body.getEndpointConfig();
+            CryptoUtil cryptoUtil = CryptoUtil.getDefaultCryptoUtil();
 
             // OAuth 2.0 backend protection: Api Key and Api Secret encryption while updating the API
             if (endpointConfig != null) {
@@ -703,7 +704,6 @@ public class ApisApiServiceImpl implements ApisApiService {
                             String apiSecret = endpointSecurityProduction.get(APIConstants
                                     .OAuthConstants.OAUTH_CLIENT_SECRET).toString();
 
-                            CryptoUtil cryptoUtil = CryptoUtil.getDefaultCryptoUtil();
                             if (!apiSecret.equals("")) {
                                 String encryptedApiSecret = cryptoUtil.encryptAndBase64Encode(apiSecret.getBytes());
                                 endpointSecurityProduction.put(APIConstants
@@ -736,7 +736,6 @@ public class ApisApiServiceImpl implements ApisApiService {
                             String apiSecret = endpointSecuritySandbox.get(APIConstants
                                     .OAuthConstants.OAUTH_CLIENT_SECRET).toString();
 
-                            CryptoUtil cryptoUtil = CryptoUtil.getDefaultCryptoUtil();
                             if (!apiSecret.equals("")) {
                                 String encryptedApiSecret = cryptoUtil.encryptAndBase64Encode(apiSecret.getBytes());
                                 endpointSecuritySandbox.put(APIConstants
